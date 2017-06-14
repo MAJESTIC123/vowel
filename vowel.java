bbd590494b5be2938bace5fd82caa2237c1eb40c
MAJESTIC123 <karthiece996@gmail.com>
2017-06-14 09:28:22 +0530

    Create vowel.java

--- vowel.java
+++ vowel.java
@@ -2,10 +2,10 @@ class vowel{
 public static void main(String[ ] args){
 char ch=args[0].charAt(0);	 		
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
-System.out.println("Entered character "+ch+" is  Vowel"); }
+System.out.println("Entered character "+ch+" is a Vowel"); }
 else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
-System.out.println("Entered character "+ch+" is Consonant");}
+System.out.println("Entered character "+ch+" is a Consonant");}
 else{
-System.out.println("Not an alphabet");}		
+System.out.println("Not a alphabet");}		
 }
 }
